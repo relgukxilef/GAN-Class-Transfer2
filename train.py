2c20fd255e85a38fcd301fe83f2ccf361d7facb3
Felix Kugler <relgukxilef@gmail.com>
2020-11-03 20:42:16 +0100

    Implement flow

--- train.py
+++ train.py
@@ -1,74 +1,24 @@
 
 import datetime, os
 import tensorflow as tf
+import tensorflow_probability as tfp
 import tqdm
 
 filters = 16
-kernel_size = 63
+kernel_size = 31
+size = 256
 
-batch_size = 8
+batch_size = 1
 
-generator = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(
-        filters, (kernel_size, 1), padding='same', use_bias=False
-    ),
-    #tf.keras.layers.ReLU(),
-    tf.keras.layers.Conv2D(filters, (1, kernel_size), padding='same'),
-    tf.keras.layers.ReLU(),
-    tf.keras.layers.Conv2D(3, (1, 1), padding='same'),
-])
+gpu = tf.config.experimental.list_physical_devices('GPU')[0]
+tf.config.experimental.set_memory_growth(gpu, True)
 
-discriminator = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(
-        filters, (kernel_size, 1), padding='same', use_bias=False
-    ),
-    #tf.keras.layers.ReLU(),
-    tf.keras.layers.Conv2D(filters, (1, kernel_size), padding='same'),
-    tf.keras.layers.ReLU(),
-    tf.keras.layers.GlobalAveragePooling2D(),
-    tf.keras.layers.Dense(filters),
-    tf.keras.layers.ReLU(),
-    tf.keras.layers.Dense(1),
-])
-
-optimizer = tf.keras.optimizers.SGD(0.01)
-#optimizer = tf.keras.optimizers.Adam()
-
-@tf.function
-def train_step(source_images, target_images, step):
-    with tf.GradientTape(persistent=True) as tape:
-        generated_images = generator(source_images)
-
-        real_output = discriminator(target_images)
-        fake_output = discriminator(generated_images)
-
-        generator_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)(
-            tf.ones_like(fake_output), fake_output
-        )
-
-        discriminator_loss = sum([
-            tf.keras.losses.BinaryCrossentropy(from_logits=True)(
-                tf.ones_like(real_output), real_output
-            ),
-            tf.keras.losses.BinaryCrossentropy(from_logits=True)(
-                tf.zeros_like(fake_output), fake_output
-            )
-        ])
-
-    optimizer.apply_gradients(zip(
-        tape.gradient(generator_loss, generator.trainable_variables), 
-        generator.trainable_variables
-    ))
-    optimizer.apply_gradients(zip(
-        tape.gradient(discriminator_loss, discriminator.trainable_variables), 
-        discriminator.trainable_variables
-    ))
 
 def load_file(file, crop=True):
     image = tf.image.decode_jpeg(tf.io.read_file(file))[:, :, :3]
     if crop:
-        image = tf.image.random_crop(image, [256, 256, 3])
-    return tf.cast(image, tf.float32) / 128 - 1
+        image = tf.image.random_crop(image, [size, size, 3])
+    return (tf.cast(image, tf.float32) / 128 - 1, 0)
 
 classes = [
     "../Datasets/safebooru_r63_256/train/male/*",
@@ -85,24 +35,157 @@ example = load_file(
 
 for folder in classes:
     dataset = tf.data.Dataset.list_files(folder)
-    dataset = dataset.shuffle(1000).repeat()
+    dataset = dataset.repeat()
     dataset = dataset.map(load_file).batch(batch_size).prefetch(8)
     datasets += [dataset]
 
-name = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+def lift(bijector, split, event_ndims):
+    f = lambda x: tfp.bijectors.Blockwise(
+        [
+            bijector(x[..., split:]),
+            tfp.bijectors.Identity(),
+        ], [split, tf.shape(x)[-1] - split]
+    )
+    return tfp.bijectors.Inline(
+        lambda x: f(x).forward(x),
+        lambda x: f(x).inverse(x),
+        lambda x: f(x).inverse_log_det_jacobian(x, event_ndims),
+        lambda x: f(x).forward_log_det_jacobian(x, event_ndims),
+        forward_min_event_ndims = event_ndims
+    )
+    
+def lu_initializer(shape, dtype=None):
+    matrix = tf.keras.initializers.Orthogonal()(shape)
+    lower_upper, permutation = tf.linalg.lu(matrix)
+    del matrix, permutation
+    return lower_upper
+
+def space_to_depth(event_shape, block_size):
+    return tfp.bijectors.Chain([
+        tfp.bijectors.Reshape([
+            event_shape[0] // block_size[0], block_size[0],
+            event_shape[1] // block_size[1], block_size[1],
+            event_shape[2]
+        ], event_shape),
+        tfp.bijectors.Transpose([0, 2, 1, 3, 4]),
+        tfp.bijectors.Reshape([
+            event_shape[0] // block_size[0], event_shape[1] // block_size[1],
+            block_size[0] * block_size[1] * event_shape[2]
+        ], [
+            event_shape[0] // block_size[0], event_shape[1] // block_size[1],
+            block_size[0], block_size[1], event_shape[2]
+        ]),
+    ])
+
+def depth_to_space(event_shape, block_size):
+    return tfp.bijectors.Invert(space_to_depth(
+        [
+            event_shape[0] * block_size[0], event_shape[1] * block_size[1], 
+            event_shape[2] // block_size[0] // block_size[1]
+        ], block_size
+    ))
+   
+class Dense(tf.keras.layers.Layer):
+    def __init__(self, outputs):
+        super().__init__()
+
+        self.matrix = self.add_weight(
+            shape=(outputs, outputs), 
+            initializer=lu_initializer
+        )
+        self.bias = self.add_weight(
+            shape=(outputs,), initializer=tf.keras.initializers.Zeros()
+        )
+
+    def bijector(self):
+        return tfp.bijectors.Chain([
+            tfp.bijectors.Affine(self.bias),
+            tfp.bijectors.MatvecLU(
+                self.matrix, [1, 2, 0]
+            )
+        ])
+
+class Block(tf.keras.layers.Layer):
+    def __init__(self):
+        super().__init__()
+
+        self.convolution = tf.keras.layers.Conv2D(
+            filters, kernel_size, 1, 'same'
+        )
+        self.dense = tf.keras.layers.Dense(1)
+
+        self.bijective_dense = Dense(3)
+
+        self.batch_norm = tfp.bijectors.BatchNormalization()
+
+    def nonlinearity(self, input):
+        shift = self.dense(tf.nn.relu(self.convolution(input)))
+        return tfp.bijectors.Affine(shift)
+
+    def bijector(self):
+        return tfp.bijectors.Chain([
+            lift(lambda x: self.nonlinearity(x), 1, 3),
+            self.bijective_dense.bijector(),
+            self.batch_norm,
+            #tfp.bijectors.Permute([1, 2, 0]),
+        ])
+
+
+class Generator(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        
+        self.blocks = [Block() for _ in range(8)]
+
+    def bijector(self):
+        return tfp.bijectors.Chain([
+            #space_to_depth([size, size, 3], [4, 4]),
+        ] + [b.bijector() for b in self.blocks] + [
+            #depth_to_space([size//4, size//4, 3*4*4], [4, 4]),
+        ])
+
+    def call(self, example):
+        distribution = tfp.distributions.Independent(
+            tfp.distributions.Normal(
+                tf.zeros_like(example), 1.0
+            ), 3
+        )
+        return tfp.distributions.TransformedDistribution(
+            distribution, self.bijector()
+        ).log_prob(example) / (size * size)
+
+    def sample(self, size):
+        distribution = tfp.distributions.Independent(
+            tfp.distributions.Normal(
+                tf.zeros(size), 1.0
+            ), 3
+        )
+        return tfp.distributions.TransformedDistribution(
+            distribution, self.bijector()
+        ).sample()
+
+model = Generator()
+
+def log_sample(epochs, logs):
+    with summary_writer.as_default():
+        fake = model.sample([4, size, size, 3]) * 0.5 + 0.5
+        tf.summary.image('fake', fake, epochs, 4)
+        del fake
+
+
+name = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M")
 summary_writer = tf.summary.create_file_writer(os.path.join("logs", name))
 
-progress = tqdm.tqdm(tf.data.Dataset.zip(tuple(datasets)))
-for step, (image_x, image_y) in enumerate(progress):
-    train_step(image_x, image_y, tf.constant(step, tf.int64))
-    
-    if step % 100 == 0:
-        with summary_writer.as_default():
-            #tf.summary.scalar('generator_loss', generator_loss, step)
-            #tf.summary.scalar('discriminator_loss', discriminator_loss, step)
-            tf.summary.image(
-                'fakes', 
-                tf.clip_by_value(generator(example[None]) * 0.5 + 0.5, 0, 1), 
-                step, 1
-            )
-    #progress.set_description("Loss: {}".format(5))
+model.compile(
+    tf.keras.optimizers.Adam(),#.SGD(1e-3), 
+    lambda y_true, log_prob: -tf.reduce_mean(log_prob)
+)
+
+model.fit(
+    datasets[1], steps_per_epoch=1000, epochs=100,
+    callbacks=[
+        tf.keras.callbacks.LambdaCallback(
+            on_epoch_end=log_sample
+        )
+    ]
+)
