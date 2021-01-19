ac26ed9316b6c821272bf6d54308c9acfe7c3a9b
Felix Kugler <relgukxilef@gmail.com>
2021-01-20 00:06:15 +0100

    Implement non-convolutional GAN

--- train.py
+++ train.py
@@ -3,41 +3,57 @@ import datetime, os
 import tensorflow as tf
 import tqdm
 
-filters = 16
-kernel_size = 63
+size = 256
+filters = 512
 
 batch_size = 8
 
-generator = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(
-        filters, (kernel_size, 1), padding='same', use_bias=False
-    ),
-    #tf.keras.layers.ReLU(),
-    tf.keras.layers.Conv2D(filters, (1, kernel_size), padding='same'),
-    tf.keras.layers.ReLU(),
-    tf.keras.layers.Conv2D(3, (1, 1), padding='same'),
-])
+gpu = tf.config.experimental.list_physical_devices('GPU')[0]
+tf.config.experimental.set_memory_growth(gpu, True)
 
-discriminator = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(
-        filters, (kernel_size, 1), padding='same', use_bias=False
-    ),
-    #tf.keras.layers.ReLU(),
-    tf.keras.layers.Conv2D(filters, (1, kernel_size), padding='same'),
-    tf.keras.layers.ReLU(),
-    tf.keras.layers.GlobalAveragePooling2D(),
-    tf.keras.layers.Dense(filters),
-    tf.keras.layers.ReLU(),
-    tf.keras.layers.Dense(1),
-])
+class Generator(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        
+        self.dense0 = tf.keras.layers.Dense(filters, use_bias=False)
+        self.dense1 = tf.keras.layers.Dense(3)
+        self.encoding = self.add_weight(
+            "encoding",
+            shape=[size, size, filters]
+        )
 
-optimizer = tf.keras.optimizers.SGD(0.01)
-#optimizer = tf.keras.optimizers.Adam()
+    def call(self, latent):
+        return self.dense1(tf.nn.relu(self.encoding + self.dense0(latent)))
+
+class Discriminator(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        
+        self.dense0 = tf.keras.layers.Dense(filters, use_bias=False)
+        self.dense1 = tf.keras.layers.Dense(filters)
+        self.dense2 = tf.keras.layers.Dense(1)
+        self.encoding = self.add_weight(
+            "encoding",
+            shape=[size, size, filters]
+        )
+
+    def call(self, image):
+        x = tf.nn.relu(self.encoding + self.dense0(image))
+        x = tf.reduce_mean(x, [-2, -3], keepdims=True)
+        x = tf.nn.relu(self.dense1(x))
+
+        return self.dense2(x)
+            
+
+optimizer = tf.keras.optimizers.RMSprop()
+
+generator = Generator()
+discriminator = Discriminator()
 
 @tf.function
 def train_step(source_images, target_images, step):
     with tf.GradientTape(persistent=True) as tape:
-        generated_images = generator(source_images)
+        generated_images = generator(tf.random.normal([1, 1, filters]))
 
         real_output = discriminator(target_images)
         fake_output = discriminator(generated_images)
@@ -67,8 +83,16 @@ def train_step(source_images, target_images, step):
 def load_file(file, crop=True):
     image = tf.image.decode_jpeg(tf.io.read_file(file))[:, :, :3]
     if crop:
-        image = tf.image.random_crop(image, [256, 256, 3])
+        image = tf.image.random_crop(image, [size, size, 3])
     return tf.cast(image, tf.float32) / 128 - 1
+
+def get_encoding_summary(model):
+    return tf.clip_by_value(
+        tf.transpose(
+            model.encoding[..., :8], [2, 0, 1]
+        )[..., None] * 0.5 + 0.5, 
+        0, 1
+    )
 
 classes = [
     "../Datasets/safebooru_r63_256/train/male/*",
@@ -96,13 +120,22 @@ progress = tqdm.tqdm(tf.data.Dataset.zip(tuple(datasets)))
 for step, (image_x, image_y) in enumerate(progress):
     train_step(image_x, image_y, tf.constant(step, tf.int64))
     
-    if step % 100 == 0:
+    if step % 200 == 0:
         with summary_writer.as_default():
             #tf.summary.scalar('generator_loss', generator_loss, step)
             #tf.summary.scalar('discriminator_loss', discriminator_loss, step)
             tf.summary.image(
                 'fakes', 
-                tf.clip_by_value(generator(example[None]) * 0.5 + 0.5, 0, 1), 
-                step, 1
+                tf.clip_by_value(
+                    generator(
+                        tf.random.normal([4, 1, 1, filters])
+                    ) * 0.5 + 0.5, 
+                    0, 1
+                ), 
+                step, 4
             )
-    #progress.set_description("Loss: {}".format(5))
+            for i, model in enumerate([generator, discriminator]):
+                encoding = get_encoding_summary(model)
+                tf.summary.image(
+                    'encoding/' + str(i), encoding, step, 8
+                )
