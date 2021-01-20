5ed409fb70edfd057b76fc421e4480c7410756ce
Felix Kugler <relgukxilef@gmail.com>
2021-01-20 03:12:55 +0100

    Implement residual GAN

--- train.py
+++ train.py
@@ -5,8 +5,12 @@ import tqdm
 
 size = 256
 filters = 512
+kernel_size = 5
+depth = 5
+block_size = 16
 
-batch_size = 8
+batch_size = 1
+optimizer = tf.keras.optimizers.RMSprop(1e-4)
 
 gpu = tf.config.experimental.list_physical_devices('GPU')[0]
 tf.config.experimental.set_memory_growth(gpu, True)
@@ -14,38 +18,68 @@ tf.config.experimental.set_memory_growth(gpu, True)
 class Generator(tf.keras.Model):
     def __init__(self):
         super().__init__()
-        
-        self.dense0 = tf.keras.layers.Dense(filters, use_bias=False)
-        self.dense1 = tf.keras.layers.Dense(3)
-        self.encoding = self.add_weight(
-            "encoding",
-            shape=[size, size, filters]
-        )
 
-    def call(self, latent):
-        return self.dense1(tf.nn.relu(self.encoding + self.dense0(latent)))
+        self.convolutions = [
+            tf.keras.layers.Conv2D(
+                filters, kernel_size, 1, 'same', activation='relu'
+            ) 
+            for _ in range(depth)
+        ]
+        self.denses = [
+            tf.keras.layers.Dense(
+                3 * block_size * block_size, 
+                kernel_initializer='zeros'
+            )
+            for _ in range(depth)
+        ]
+
+    def call(self, image):
+        image = tf.nn.space_to_depth(image, block_size)
+        for c, d in zip(self.convolutions, self.denses):
+            image += d(c(image))
+        image = tf.nn.depth_to_space(image, block_size)
+        return image
+
+    def activations(self, image):
+        images = []
+        image = tf.nn.space_to_depth(image, block_size)
+        for i, (c, d) in enumerate(zip(self.convolutions, self.denses)):
+            image += d(c(image))
+            images += [tf.nn.depth_to_space(image, block_size)]
+        return images
 
 class Discriminator(tf.keras.Model):
     def __init__(self):
         super().__init__()
         
-        self.dense0 = tf.keras.layers.Dense(filters, use_bias=False)
-        self.dense1 = tf.keras.layers.Dense(filters)
-        self.dense2 = tf.keras.layers.Dense(1)
-        self.encoding = self.add_weight(
-            "encoding",
-            shape=[size, size, filters]
-        )
+        self.convolutions = [
+            tf.keras.layers.Conv2D(
+                filters, kernel_size, 1, 'same', activation='relu'
+            ) 
+            for _ in range(depth)
+        ]
+        self.denses = [
+            tf.keras.layers.Dense(
+                3 * block_size * block_size, kernel_initializer='zeros'
+            ) 
+            for _ in range(depth)
+        ]
+        self.dense = tf.keras.layers.Dense(1)
 
     def call(self, image):
-        x = tf.nn.relu(self.encoding + self.dense0(image))
-        x = tf.reduce_mean(x, [-2, -3], keepdims=True)
-        x = tf.nn.relu(self.dense1(x))
+        image = tf.nn.space_to_depth(image, block_size)
+        for c, d in zip(self.convolutions, self.denses):
+            image += d(c(image))
+        return self.dense(tf.keras.layers.GlobalAveragePooling2D()(image))
 
-        return self.dense2(x)
+    def activations(self, image):
+        images = []
+        image = tf.nn.space_to_depth(image, block_size)
+        for c, d in zip(self.convolutions, self.denses):
+            image += d(c(image))
+            images += [tf.nn.depth_to_space(image, block_size)]
+        return images
             
-
-optimizer = tf.keras.optimizers.RMSprop()
 
 generator = Generator()
 discriminator = Discriminator()
@@ -53,7 +87,7 @@ discriminator = Discriminator()
 @tf.function
 def train_step(source_images, target_images, step):
     with tf.GradientTape(persistent=True) as tape:
-        generated_images = generator(tf.random.normal([1, 1, filters]))
+        generated_images = generator(source_images)
 
         real_output = discriminator(target_images)
         fake_output = discriminator(generated_images)
@@ -80,19 +114,18 @@ def train_step(source_images, target_images, step):
         discriminator.trainable_variables
     ))
 
+    accuracy = 0.5 * (
+        tf.reduce_mean(tf.cast(tf.greater(real_output, 0), tf.float32)) +
+        tf.reduce_mean(tf.cast(tf.less(fake_output, 0), tf.float32))
+    )
+
+    return accuracy
+
 def load_file(file, crop=True):
     image = tf.image.decode_jpeg(tf.io.read_file(file))[:, :, :3]
     if crop:
         image = tf.image.random_crop(image, [size, size, 3])
     return tf.cast(image, tf.float32) / 128 - 1
-
-def get_encoding_summary(model):
-    return tf.clip_by_value(
-        tf.transpose(
-            model.encoding[..., :8], [2, 0, 1]
-        )[..., None] * 0.5 + 0.5, 
-        0, 1
-    )
 
 classes = [
     "../Datasets/safebooru_r63_256/train/male/*",
@@ -105,7 +138,7 @@ example = load_file(
     "../Datasets/safebooru_r63_256/train/male/" +
     "00fdb833c64d7824edaa555277e494331b3882891f9422c6bca07611a5193b5f.png", 
     False
-)
+)[:362//block_size*block_size, :, :]
 
 for folder in classes:
     dataset = tf.data.Dataset.list_files(folder)
@@ -116,26 +149,30 @@ for folder in classes:
 name = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 summary_writer = tf.summary.create_file_writer(os.path.join("logs", name))
 
+def scale_and_clip(image):
+    return tf.clip_by_value(image * 0.5 + 0.5, 0, 1)
+
+accuracy = 0
 progress = tqdm.tqdm(tf.data.Dataset.zip(tuple(datasets)))
 for step, (image_x, image_y) in enumerate(progress):
-    train_step(image_x, image_y, tf.constant(step, tf.int64))
+    accuracy += train_step(image_x, image_y, tf.constant(step, tf.int64))
     
     if step % 200 == 0:
         with summary_writer.as_default():
             #tf.summary.scalar('generator_loss', generator_loss, step)
             #tf.summary.scalar('discriminator_loss', discriminator_loss, step)
+            tf.summary.scalar('discriminator_accuracy', accuracy / 200, step)
             tf.summary.image(
                 'fakes', 
-                tf.clip_by_value(
-                    generator(
-                        tf.random.normal([4, 1, 1, filters])
-                    ) * 0.5 + 0.5, 
-                    0, 1
-                ), 
-                step, 4
+                scale_and_clip(generator(example[None])), 
+                step, 1
             )
-            for i, model in enumerate([generator, discriminator]):
-                encoding = get_encoding_summary(model)
+            for name, model in [("g", generator), ("d", discriminator)]:
                 tf.summary.image(
-                    'encoding/' + str(i), encoding, step, 8
+                    name + "_activation", 
+                    scale_and_clip(
+                        tf.concat(model.activations(example[None]), 0)
+                    ), 
+                    step, 8
                 )
+        accuracy = 0
