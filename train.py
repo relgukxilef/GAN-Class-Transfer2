972d270818eac777f04a5e7bb7e51b626f2af8dc
Felix Kugler <relgukxilef@gmail.com>
2021-05-15 11:53:37 +0200

    Implement convolution over entire image

--- train.py
+++ train.py
@@ -4,13 +4,13 @@ import tensorflow as tf
 
 import tensorflow.keras.mixed_precision.experimental
 
-filters = 1024
+filters = 512
 color_size = 4
 size = 128
 layers = 1
 
 batch_size = 8
-steps_per_epoch = 4000
+steps_per_epoch = 10
 
 gpu = tf.config.experimental.list_physical_devices('GPU')[0]
 tf.config.experimental.set_memory_growth(gpu, True)
@@ -37,21 +37,9 @@ def categorical_sample(logits):
 class Generator(tf.keras.Model):
     def __init__(self):
         super().__init__()
-        # TODO: dense_in doesn't need bias
-        self.dense_in = [
-            tf.keras.layers.Dense(filters, use_bias=False) 
-            for _ in range(layers)
-        ]
+        self.length = size * size * 3
+        self.dense_in = tf.keras.layers.Conv1D(filters, self.length)
         self.dense_final = tf.keras.layers.Dense(color_size)
-        self.positional_encoding = tf.Variable(
-            initial_value=tf.random.normal([size * size * 3, filters]),
-            trainable=True,
-        )
-        length = size * size * 3
-        self.factor = tf.cast(
-            1 / tf.range(1, length + 1, dtype=tf.float32)[:, None], 
-            dtype=tf.float16
-        )
 
 
     @tf.function
@@ -60,27 +48,19 @@ class Generator(tf.keras.Model):
 
         shape = tf.shape(example)
 
-        length = tf.reduce_prod(shape[-3:])
-
         x = tf.reshape(
             example, 
-            tf.concat([shape[:-3], [length, 1]], 0)
+            tf.concat([shape[:-3], [self.length, 1]], 0)
         )
         # x[batch, height * width * channel, 1]
 
-        x = x * 2 - 1
-        
-        # TODO: maybe try using an embedding for the discrete input
-        x = self.dense_in[0](x)
+        x = tf.pad(x, [[0, 0], [self.length, 0], [0, 0]])[:, :-1, :]
 
-        x += tf.cast(self.positional_encoding, dtype=x.dtype)
+        x = self.dense_in(x)
+        # TODO: try this with FFT
 
         x = tf.nn.relu(x)
         
-        x = tf.math.cumsum(x, -2) * self.factor # prefix mean
-        #x = tf.concat([x, tf.math.cumsum(x, -2) / size / size / 3], -1)
-        #x = tf.math.l2_normalize(tf.math.cumsum(x, -2), -1)
-
         x = self.dense_final(x)
 
         x = tf.reshape(
@@ -92,24 +72,15 @@ class Generator(tf.keras.Model):
     @tf.function
     def sample(self):
         @tf.function
-        def value(a, element):
-            x, sums = a
-            p, f = element
-            sums = sums[:]
+        def value(buffer):
+            x = buffer
 
             x = x * 2 - 1
 
-            x = self.dense_in[0](x)
-
-            x += p
+            x = self.dense_in(x)
 
             x = tf.nn.relu(x)
 
-            sums[0] += x
-            x = sums[0] * f
-            #x = tf.math.l2_normalize(sums[0], -1)
-            #x = tf.concat([x, sums[0] / size / size / 3], -1)
-            
             x = self.dense_final(x)
 
             x = (
@@ -117,15 +88,14 @@ class Generator(tf.keras.Model):
                 color_size
             )
 
-            return (x, sums)
+            x = tf.concat([buffer[..., 1:, :], x], -2)
 
-        fake = tf.scan(
-            value, (tf.cast(self.positional_encoding, tf.float16), self.factor), 
-            (
-                tf.zeros([1, 1], dtype=tf.float16), 
-                [tf.zeros([1, filters], dtype=tf.float16)] * layers
-            )
-        )[0]
+            return (x,)
+
+        fake = tf.while_loop(
+            lambda x: True, value, (tf.zeros([1, self.length, 1], tf.float16),), 
+            maximum_iterations=self.length
+        )
 
         x = tf.reshape(fake, [size, size, 3])
 
