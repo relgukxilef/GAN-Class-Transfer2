b5f49c5d09a06ad95c20913619d0e3603775029e
Felix Kugler <relgukxilef@gmail.com>
2021-08-02 17:22:38 +0200

    Use bilinear upsampling and pooling

--- train.py
+++ train.py
@@ -1,29 +1,28 @@
 
 import datetime, os
 import tensorflow as tf
-#import tensorflow_probability as tfp
 import tqdm
 
 size = 256
-pixel_size = 64
-code_size = 256
+pixel_size = 128
+code_size = 128
 
-batch_size = 8
+batch_size = 1
 
 gpu = tf.config.list_physical_devices('GPU')[0]
 tf.config.experimental.set_memory_growth(gpu, True)
 
 policy = tf.keras.mixed_precision.Policy('mixed_float16')
-tf.keras.mixed_precision.experimental.set_policy(policy)
+#tf.keras.mixed_precision.experimental.set_policy(policy)
 
 #optimizer = tf.keras.optimizers.Adam()
-optimizer_g = tf.keras.optimizers.RMSprop(1e-4)
-optimizer_d = tf.keras.optimizers.RMSprop(1e-4)
+optimizer_g = tf.keras.optimizers.Adam(1e-5, 0, 0.99)
+optimizer_d = tf.keras.optimizers.Adam(1e-5, 0, 0.99)
 
-optimizer_g, optimizer_d = [
-    tf.keras.mixed_precision.LossScaleOptimizer(optimizer)
-    for optimizer in [optimizer_g, optimizer_d]
-]
+#optimizer_g, optimizer_d = [
+#    tf.keras.mixed_precision.LossScaleOptimizer(optimizer)
+#    for optimizer in [optimizer_g, optimizer_d]
+#]
 
 class PairwiseSum(tf.keras.layers.Layer):
     def __init__(self):
@@ -43,6 +42,7 @@ class Residual(tf.keras.layers.Layer):
         self.module.build(input_shape)
 
     def call(self, input):
+        return self.module(input)
         return input + self.module(input)
 
 class Block(tf.keras.layers.Layer):
@@ -54,10 +54,10 @@ class Block(tf.keras.layers.Layer):
     def build(self, input_shape):
         self.module = tf.keras.Sequential([
             tf.keras.layers.Conv2D(
-                self.filters, 4, 1, 'same', activation='relu'
+                self.filters, 3, 1, 'same', activation='relu'
             ),
             tf.keras.layers.Conv2D(
-                input_shape[-1], 3, 1, 'same', kernel_initializer='zeros'
+                input_shape[-1], 3, 1, 'same'#, kernel_initializer='zeros'
             )
         ])
         self.module.build(input_shape)
@@ -72,7 +72,7 @@ class UpShuffle(tf.keras.layers.Layer):
         self.size = size
 
     def call(self, input):
-        return tf.nn.depth_to_space(input, self.size)
+        return tf.keras.layers.UpSampling2D()(input)
 
 class DownShuffle(tf.keras.layers.Layer):
     def __init__(self, size):
@@ -81,12 +81,13 @@ class DownShuffle(tf.keras.layers.Layer):
         self.size = size
 
     def call(self, input):
-        return tf.nn.space_to_depth(input, self.size)
+        return tf.keras.layers.AveragePooling2D()(input)
 
 generator = tf.keras.Sequential([
     tf.keras.layers.Dense(code_size, activation='relu'),
-    tf.keras.layers.Dense(4 * 256 * 256),
-    tf.keras.layers.Reshape((8, 8, -1)),
+    tf.keras.layers.Dense(code_size, activation='relu'),
+    tf.keras.layers.Dense(8 * 8 * 256),
+    tf.keras.layers.Reshape((8, 8, 256)),
     # 8x8
     Residual(Block(pixel_size)), UpShuffle(2),
     Residual(Block(pixel_size)), UpShuffle(2),
@@ -101,7 +102,7 @@ generator = tf.keras.Sequential([
 
 discriminator = tf.keras.Sequential([
     tf.keras.layers.Dense(pixel_size, activation='relu'),
-    tf.keras.layers.Dense(3),
+    tf.keras.layers.Dense(256),
     # 256x256
     Residual(Block(pixel_size)), DownShuffle(2),
     Residual(Block(pixel_size)), DownShuffle(2),
@@ -112,7 +113,8 @@ discriminator = tf.keras.Sequential([
     # 8x8
     tf.keras.layers.Reshape((-1,)),
     tf.keras.layers.Dense(code_size, activation='relu'),
-    PairwiseSum(),
+    #PairwiseSum(),
+    tf.keras.layers.Dense(code_size, activation='relu'),
     tf.keras.layers.Dense(code_size, activation='relu'),
     tf.keras.layers.Dense(1, kernel_initializer='zeros'),
 ])
@@ -132,23 +134,23 @@ def train_step(source_images, target_images, step):
         #generator_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)(
         #    tf.ones_like(fake_output), fake_output
         #)
-        #generator_loss = tf.keras.losses.MeanSquaredError()(0, fake_output)
-        generator_loss = tf.reduce_mean(
-            -0.5 * fake_output + tf.math.softplus(fake_output)
-        )
-
-        discriminator_loss = 0.5 * sum([
-            tf.keras.losses.BinaryCrossentropy(from_logits=True)(
-                tf.ones_like(real_output), real_output
-            ),
-            tf.keras.losses.BinaryCrossentropy(from_logits=True)(
-                tf.zeros_like(fake_output), fake_output
-            )
-        ])
-        #discriminator_loss = 0.5 * (
-        #    tf.keras.losses.MeanSquaredError()(1, real_output) +
-        #    tf.keras.losses.MeanSquaredError()(-1, fake_output)
+        generator_loss = tf.keras.losses.MeanSquaredError()(0, fake_output)
+        #generator_loss = tf.reduce_mean(
+        #    -0.5 * fake_output + tf.math.softplus(fake_output)
         #)
+
+        #discriminator_loss = 0.5 * sum([
+        #    tf.keras.losses.BinaryCrossentropy(from_logits=True)(
+        #        tf.ones_like(real_output), real_output
+        #    ),
+        #    tf.keras.losses.BinaryCrossentropy(from_logits=True)(
+        #        tf.zeros_like(fake_output), fake_output
+        #    )
+        #])
+        discriminator_loss = 0.5 * (
+            tf.keras.losses.MeanSquaredError()(1, real_output) +
+            tf.keras.losses.MeanSquaredError()(-1, fake_output)
+        )
 
     optimizer_g.apply_gradients(zip(
         tape.gradient(generator_loss, generator.trainable_variables), 
