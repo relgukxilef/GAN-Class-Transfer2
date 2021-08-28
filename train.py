288715bd97f616b072d697bc9223528a3f55251d
Felix Kugler <relgukxilef@gmail.com>
2021-08-28 22:12:16 +0200

    Implement diffusion model with predicted alphas

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 
 import datetime, os
 import tensorflow as tf
-import tqdm
+import tensorflow_probability as tfp
 
 size = 256
 pixel_size = 128
@@ -15,22 +15,9 @@ tf.config.experimental.set_memory_growth(gpu, True)
 policy = tf.keras.mixed_precision.Policy('mixed_float16')
 #tf.keras.mixed_precision.experimental.set_policy(policy)
 
-#optimizer = tf.keras.optimizers.Adam()
-optimizer_g = tf.keras.optimizers.Adam(1e-5, 0, 0.99)
-optimizer_d = tf.keras.optimizers.Adam(1e-5, 0, 0.99)
+optimizer = tf.keras.optimizers.Adam()
 
-#optimizer_g, optimizer_d = [
-#    tf.keras.mixed_precision.LossScaleOptimizer(optimizer)
-#    for optimizer in [optimizer_g, optimizer_d]
-#]
-
-class PairwiseSum(tf.keras.layers.Layer):
-    def __init__(self):
-        super().__init__()
-
-    def call(self, input):
-        a, b = tf.split(input, 2, 0)
-        return a + b
+#optimizer = tf.keras.mixed_precision.LossScaleOptimizer(optimizer)
 
 class Residual(tf.keras.layers.Layer):
     def __init__(self, module):
@@ -42,22 +29,25 @@ class Residual(tf.keras.layers.Layer):
         self.module.build(input_shape)
 
     def call(self, input):
-        return self.module(input)
+        #return self.module(input)
         return input + self.module(input)
 
 class Block(tf.keras.layers.Layer):
-    def __init__(self, filters):
+    def __init__(self, filters, dilation = 1):
         super().__init__()
 
         self.filters = filters
+        self.dilation = dilation
 
     def build(self, input_shape):
         self.module = tf.keras.Sequential([
             tf.keras.layers.Conv2D(
-                self.filters, 3, 1, 'same', activation='relu'
+                self.filters, 3, 1, 'same', activation='relu', 
+                dilation_rate=(self.dilation, self.dilation)
             ),
             tf.keras.layers.Conv2D(
-                input_shape[-1], 3, 1, 'same'#, kernel_initializer='zeros'
+                input_shape[-1], 3, 1, 'same', #kernel_initializer='zeros',
+                dilation_rate=(self.dilation, self.dilation)
             )
         ])
         self.module.build(input_shape)
@@ -65,112 +55,99 @@ class Block(tf.keras.layers.Layer):
     def call(self, input):
         return self.module(input)
 
-class UpShuffle(tf.keras.layers.Layer):
-    def __init__(self, size):
+class Encoder(tf.keras.layers.Layer):
+    def __init__(self):
         super().__init__()
 
-        self.size = size
+        self.rgb_dense = tf.keras.layers.Dense(pixel_size, use_bias=False)
+        self.scale_dense = tf.keras.layers.Dense(pixel_size)
+        self.output_dense = tf.keras.layers.Dense(pixel_size)
+
+    def build(self, input_shape):
+        rgb, scale = input_shape
+        self.rgb_dense.build(rgb)
+        self.scale_dense.build(scale)
+        self.output_dense.build([pixel_size])
 
     def call(self, input):
-        return tf.keras.layers.UpSampling2D(interpolation='bilinear')(input)
+        rgb, scale = input
+        return self.output_dense(tf.nn.relu(
+            self.rgb_dense(rgb) + 
+            self.scale_dense(scale)
+        ))
 
-class DownShuffle(tf.keras.layers.Layer):
-    def __init__(self, size):
+class Decoder(tf.keras.layers.Layer):
+    def __init__(self):
         super().__init__()
 
-        self.size = size
+    def call(self, input):
+        rgb, scale = tf.split(input, [3, 1], -1)
+        scale = tf.reduce_mean(scale, axis=[-1, -2, -3], keepdims=True)
+        return rgb, scale
+
+class Noiser(tf.keras.layers.Layer):
+    def __init__(self):
+        super().__init__()
 
     def call(self, input):
-        return tf.keras.layers.AveragePooling2D()(input)
-
-generator = tf.keras.Sequential([
-    tf.keras.layers.Dense(code_size, activation='relu'),
-    tf.keras.layers.Dense(code_size, activation='relu'),
-    tf.keras.layers.Dense(8 * 8 * 256),
-    tf.keras.layers.Reshape((8, 8, 256)),
-    # 8x8
-    Residual(Block(pixel_size)), UpShuffle(2),
-    Residual(Block(pixel_size)), UpShuffle(2),
-    Residual(Block(pixel_size)), UpShuffle(2),
-    Residual(Block(pixel_size)), UpShuffle(2),
-    Residual(Block(pixel_size)), UpShuffle(2),
-    Residual(Block(pixel_size)), 
-    # 256x256
-    tf.keras.layers.Dense(pixel_size, activation='relu'),
-    tf.keras.layers.Dense(3),
-])
-
-discriminator = tf.keras.Sequential([
-    tf.keras.layers.Dense(pixel_size, activation='relu'),
-    tf.keras.layers.Dense(256),
-    # 256x256
-    Residual(Block(pixel_size)), DownShuffle(2),
-    Residual(Block(pixel_size)), DownShuffle(2),
-    Residual(Block(pixel_size)), DownShuffle(2),
-    Residual(Block(pixel_size)), DownShuffle(2),
-    Residual(Block(pixel_size)), DownShuffle(2),
-    Residual(Block(pixel_size)), 
-    # 8x8
-    tf.keras.layers.Reshape((-1,)),
-    tf.keras.layers.Dense(code_size, activation='relu'),
-    #PairwiseSum(),
-    tf.keras.layers.Dense(code_size, activation='relu'),
-    tf.keras.layers.Dense(code_size, activation='relu'),
-    tf.keras.layers.Dense(1, kernel_initializer='zeros'),
-])
+        log_scale = tf.random.uniform(
+            tf.shape(input)[:-3], 
+            tf.math.log(1.0/256), tf.math.log(1.0)
+        )[..., None, None, None]
+        scale = tf.exp(log_scale)
+        noised = (
+            input * tf.sqrt(1 - tf.square(scale)) + 
+            tf.random.normal(tf.shape(input)) * scale
+        )
+        return noised, log_scale
 
 @tf.function
-def train_step(source_images, target_images, step):
-    target_images = tf.image.random_flip_left_right(target_images)
+def identity(y_true, y_pred):
+    return tf.reduce_mean(y_pred)
 
-    with tf.GradientTape(persistent=True) as tape:
-        generated_images = generator(
-            tf.random.normal((batch_size, code_size))
-        )
+class Denoiser(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
 
-        tf.debugging.check_numerics(generated_images, "generated_images")
+        self.encoder = Encoder()
+        self.middle = tf.keras.Sequential([
+            Residual(Block(pixel_size, 64)),
+            Residual(Block(pixel_size, 32)),
+            Residual(Block(pixel_size, 16)),
+            Residual(Block(pixel_size, 8)),
+            Residual(Block(pixel_size, 4)),
+            Residual(Block(pixel_size, 2)),
+            Residual(Block(pixel_size)), 
+            tf.keras.layers.Dense(pixel_size, activation='relu'),
+            tf.keras.layers.Dense(4),
+        ])
+        self.decoder = Decoder()
 
-        real_output = discriminator(target_images)
-        fake_output = discriminator(generated_images)
+    def call(self, input):
+        return self.decoder(self.middle(self.encoder(input)))
 
-        tf.debugging.check_numerics(real_output, "real_output")
-        tf.debugging.check_numerics(fake_output, "fake_output")
+class Trainer(tf.keras.Model):
+    def __init__(self, denoiser):
+        super().__init__()
 
-        #generator_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)(
-        #    tf.ones_like(fake_output), fake_output
-        #)
-        generator_loss = tf.keras.losses.MeanSquaredError()(0, fake_output)
-        #generator_loss = tf.reduce_mean(
-        #    -0.5 * fake_output + tf.math.softplus(fake_output)
-        #)
+        self.noiser = Noiser()
+        self.denoiser = denoiser
 
-        #discriminator_loss = 0.5 * sum([
-        #    tf.keras.losses.BinaryCrossentropy(from_logits=True)(
-        #        tf.ones_like(real_output), real_output
-        #    ),
-        #    tf.keras.losses.BinaryCrossentropy(from_logits=True)(
-        #        tf.zeros_like(fake_output), fake_output
-        #    )
-        #])
-        discriminator_loss = 0.5 * (
-            tf.keras.losses.MeanSquaredError()(1, real_output) +
-            tf.keras.losses.MeanSquaredError()(-1, fake_output)
-        )
+    def call(self, input):
+        rgb, log_scale = self.denoiser(self.noiser(input))
+        return -tfp.distributions.Normal(
+            rgb, tf.exp(log_scale)
+        ).log_prob(input)
 
-    optimizer_g.apply_gradients(zip(
-        tape.gradient(generator_loss, generator.trainable_variables), 
-        generator.trainable_variables
-    ))
-    optimizer_d.apply_gradients(zip(
-        tape.gradient(discriminator_loss, discriminator.trainable_variables), 
-        discriminator.trainable_variables
-    ))
+denoiser = Denoiser()
+trainer = Trainer(denoiser)
 
 def load_file(file, crop=True):
     image = tf.image.decode_jpeg(tf.io.read_file(file))[:, :, :3]
     if crop:
         image = tf.image.random_crop(image, [size, size, 3])
-    return tf.cast(image, tf.float32) / 128 - 1
+    image = tf.cast(image, tf.float32) / 128 - 1
+    return image, image
 
 classes = [
     "../Datasets/safebooru_r63_256/train/male/*",
@@ -183,7 +160,7 @@ example = load_file(
     "../Datasets/safebooru_r63_256/train/male/" +
     "00fdb833c64d7824edaa555277e494331b3882891f9422c6bca07611a5193b5f.png"
 )
-example = tf.random.normal((4, code_size))
+example = tf.random.normal((4, size, size, 3))
 
 for folder in classes:
     dataset = tf.data.Dataset.list_files(folder)
@@ -191,25 +168,40 @@ for folder in classes:
     dataset = dataset.map(load_file).batch(batch_size).prefetch(8)
     datasets += [dataset]
 
-name = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-summary_writer = tf.summary.create_file_writer(os.path.join("logs", name))
+@tf.function
+def log_sample(epochs, logs):
+    with summary_writer.as_default():
+        step = example, tf.math.log(1.0)[None, None, None, None]
+        for i in range(20):
+            fake, log_scale = denoiser(step)
+            step = (
+                tf.random.normal(example.shape, fake, tf.exp(log_scale)), 
+                log_scale
+            )
+        tf.summary.image('fake', fake * 0.5 + 0.5, epochs, 4)
+        tf.summary.image('sample', step[0] * 0.5 + 0.5, epochs, 4)
+        tf.summary.histogram('log_scale', log_scale, epochs)
 
-progress = tqdm.tqdm(tf.data.Dataset.zip(tuple(datasets)))
-try:
-    for step, (image_x, image_y) in enumerate(progress):
-        train_step(image_x, image_y, tf.constant(step, tf.int64))
-        
-        if step % 100 == 0:
-            with summary_writer.as_default():
-                fakes = generator(example)
-                tf.summary.image(
-                    'fakes', 
-                    tf.clip_by_value(
-                        fakes * 0.5 + 0.5, 
-                        0, 1
-                    ), 
-                    step, 4
-                )
-                tf.summary.histogram('fake_score', discriminator(fakes), step)
-finally:
-    pass
+if __name__ == "__main__":
+    name = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    summary_writer = tf.summary.create_file_writer(os.path.join("logs", name))
+
+    dataset_example = next(iter(datasets[1]))[0]
+    loss = identity(
+        dataset_example, trainer(dataset_example)
+    )
+    del loss, dataset_example
+
+    trainer.compile(
+        tf.keras.optimizers.SGD(1e-4), 
+        identity
+    )
+
+    trainer.fit(
+        datasets[1], steps_per_epoch=1000, epochs=100,
+        callbacks=[
+            tf.keras.callbacks.LambdaCallback(
+                on_epoch_begin=log_sample
+            )
+        ]
+    )
