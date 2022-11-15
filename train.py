fdd162165384227143e3544fe1c84b94ceab300b
Felix Kugler <relgukxilef@gmail.com>
2022-11-15 15:43:55 +0100

    Implement forward diffusion

--- train.py
+++ train.py
@@ -2,8 +2,8 @@
 import datetime, os
 import tensorflow as tf
 
-#dataset_pattern = "../Datasets/safebooru_r63_256/train/female/*.png"
-dataset_pattern = "D:/Felix/Downloads/danbooru2020/256px/*/*"
+dataset_pattern = "../Datasets/safebooru_r63_256/train/female/*.png"
+#dataset_pattern = "D:/Felix/Downloads/danbooru2020/256px/*/*"
 #dataset_pattern = \
 #"D:/Felix/Documents/Python/PyTorch Projects/Datasets/saki_castle/256px/*.jpg"
 
@@ -12,7 +12,7 @@ example_image_path = "../Datasets/safebooru_r63_256/test/female/"\
 #example_image_path = "D:/Felix/Documents/Python/PyTorch Projects/Datasets/"\
 #"saki_castle/256px/1039 saki_castle.jpg"
 
-#dataset_pattern = example_image_path
+dataset_pattern = example_image_path
 
 size = 256
 pixel_size = 128 * 1
@@ -23,13 +23,12 @@ octaves = 7 # bottleneck = 2x2
 batch_size = 8
 steps = 50
 
-min_noise = 0.5
-
 residual = False
 concat = True
 
 predict_x = False # as opposed to epsilon
 predict_scaled_epsilon = False
+prediction_weighting = False
 ordinary_differential_equation = False
 
 mixed_precision = False
@@ -46,13 +45,20 @@ if mixed_precision:
 def sign_gradient(gradient):
     return [(tf.sign(g), v) for g, v in gradient]
 
-#optimizer = tf.keras.optimizers.SGD(0.05)#, 0.5, True)
+#optimizer = tf.keras.optimizers.SGD(0.25, 0.5, True)
+optimizer = tf.keras.optimizers.SGD(
+    tf.keras.optimizers.schedules.InverseTimeDecay(2.0, 10_000, 1)
+)
 #optimizer = tf.keras.optimizers.SGD(
 #    0.0001,#, 0.9, True, 
 #    gradient_transformers=[sign_gradient]
 #)
 #optimizer = tf.keras.optimizers.Adam(1e-4)
-optimizer = tf.keras.optimizers.RMSprop(1e-4)
+#optimizer = tf.keras.optimizers.RMSprop(
+#    tf.keras.optimizers.schedules.InverseTimeDecay(1e-5, 10_000, 1)
+#)
+
+regularizer = tf.keras.regularizers.l2(1e-6)
 
 if mixed_precision:
     optimizer = tf.keras.mixed_precision.LossScaleOptimizer(optimizer)
@@ -64,14 +70,17 @@ def alpha_dash(t):
     #return (2**8 - 2**8**t) / (256 * 2**8**t - 2**8**t + 2**8)
     #return (256*256)**(-1*t)
     #return tf.cos(pi / 2 * t)**2
+    #return (1 - t)**4
     return (1 - t)**2
-    #return (1 - t)
+
+test_step = 25
 
 class Residual(tf.keras.layers.Layer):
-    def __init__(self, module):
+    def __init__(self, module, highway = lambda x: x):
         super().__init__()
 
         self.module = module
+        self.highway = highway
 
     def build(self, input_shape):
         #self.module.build(input_shape)
@@ -84,7 +93,10 @@ class Residual(tf.keras.layers.Layer):
             return input + self.dense(self.module(input))
         elif concat:
             return tf.concat(
-                [tf.cast(self.module(input), input.dtype), input], -1
+                [
+                    tf.cast(self.module(input), input.dtype), 
+                    self.highway(input)
+                ], -1
             )
         else:
             return self.module(input)
@@ -100,7 +112,9 @@ class Block(tf.keras.layers.Layer):
             tf.keras.Sequential([
                 tf.keras.layers.Conv2D(
                     self.filters, 3, 1, 'same', 
-                    kernel_initializer='he_uniform', activation='relu'
+                    kernel_initializer='he_uniform', activation='relu', 
+                    kernel_regularizer=regularizer,
+                    bias_regularizer=regularizer
                 ),
             ]) for i in range(block_depth)
         ])
@@ -114,7 +128,9 @@ class UpShuffle(tf.keras.layers.Layer):
         super().__init__()
         self.convolution = tf.keras.layers.Conv2DTranspose(
             filters, 4, 2, 'same', kernel_initializer='he_uniform', 
-            activation='relu'
+            activation='relu', 
+            kernel_regularizer=regularizer,
+            bias_regularizer=regularizer
         )
 
     def call(self, input):
@@ -125,7 +141,9 @@ class DownShuffle(tf.keras.layers.Layer):
         super().__init__()
         self.convolution = tf.keras.layers.Conv2D(
             filters, 4, 2, 'same', kernel_initializer='he_uniform', 
-            activation='relu'
+            activation='relu', 
+            kernel_regularizer=regularizer,
+            bias_regularizer=regularizer
         )
 
     def call(self, input):
@@ -145,9 +163,9 @@ class Denoiser(tf.keras.Model):
             self.middle = Residual(
                 tf.keras.Sequential([
                     DownShuffle(filters),
-                    Block(filters),
+                    #Block(filters),
                     self.middle, 
-                    Block(filters),
+                    #Block(filters),
                     UpShuffle(min(pixel_size * 2**i // 2, max_size)),
                 ])
             )
@@ -159,12 +177,22 @@ class Denoiser(tf.keras.Model):
             #    pixel_size, kernel_initializer='he_uniform', activation='relu'
             #),
             tf.keras.layers.Dense(
-                3#, kernel_initializer='zeros'
+                3 * steps, #kernel_initializer='zeros', 
+                kernel_regularizer=regularizer,
+                bias_regularizer=regularizer
             ),
+            tf.keras.layers.Reshape((size, size, steps, 3)),
         ])
 
     def call(self, input):
-        x_theta = self.middle(input)
+        x, t = input
+        t = t[..., 0]
+        t = tf.broadcast_to(t, tf.shape(x)[:-1])
+        prediction = self.middle(x)
+        x_theta = tf.gather(
+            # [batch, width, height, steps, channels]
+            prediction, t - 1, batch_dims=3
+        )
         return x_theta
 
 class Trainer(tf.keras.Model):
@@ -174,17 +202,19 @@ class Trainer(tf.keras.Model):
         self.denoiser = denoiser
 
     def call(self, x):
-        t = tf.random.uniform(
-            tf.shape(x)[:-3], minval=1.0, maxval=steps, dtype=x.dtype
+        t_int = tf.random.uniform(
+            tf.shape(x)[:-3], minval=1, maxval=steps + 1, dtype=tf.int32
         )[..., None, None, None]
         epsilon = tf.random.normal(tf.shape(x), dtype=x.dtype)
+
+        t = tf.cast(t_int, x.dtype)
 
         noised = (
             x * alpha_dash(t)**0.5 + 
             epsilon * (1 - alpha_dash(t))**0.5
         )
 
-        prediction = self.denoiser(noised)
+        prediction = self.denoiser((noised, t_int))
 
         if ordinary_differential_equation:
             target = (
@@ -197,6 +227,10 @@ class Trainer(tf.keras.Model):
             target = epsilon
             if predict_scaled_epsilon:
                 target *= (1 - alpha_dash(t))**0.5
+
+            if prediction_weighting:
+                target *= (1 - alpha_dash(t))**0.5
+                prediction *= (1 - alpha_dash(t))**0.5
 
         def dct2d(x):
             frequency_weights = 1.0 / tf.range(1, size + 1, dtype=x.dtype)
@@ -252,6 +286,11 @@ datasets = []
 example_image = tf.cast(load_file(example_image_path, True), tf.float32)
 example = tf.random.normal((1, 2, size, size, 3), dtype=tf.float32)
 
+bits_per_pixel = 3
+dictionary = tf.random.normal(
+    (size, size, 2**bits_per_pixel, 3), dtype=tf.float32
+)
+
 for folder in classes:
     datasets += [
         tf.data.Dataset.list_files(folder)
@@ -264,7 +303,7 @@ for folder in classes:
 
 def log_sample(epochs, logs):
     with summary_writer.as_default():
-        image_factor = 0.5
+        image_factor = alpha_dash(test_step)
         if ordinary_differential_equation:
             # need adjascent alpha_dash values
             image_factor = alpha_dash(steps / 2)**0.5
@@ -273,7 +312,9 @@ def log_sample(epochs, logs):
             example[0, :1, ...] * (1 - image_factor)**0.5
         )
         prediction = tf.cast(
-            denoiser(tf.cast(noised, preferred_type)), tf.float32
+            denoiser((tf.cast(noised, preferred_type), 
+            tf.constant([test_step]))), 
+            tf.float32
         )
         if ordinary_differential_equation:
             denoised = (
@@ -301,12 +342,92 @@ def log_sample(epochs, logs):
         )
         del denoised
 
-        fake = example[0, ...]
+        # forward diffusion
+        if True:
+            x_theta = example_image[0][None]
+            epsilon_theta = x_theta # might be close enough
+
+            for t_value in reversed(range(steps, 0, -1)): # smallest t = 1
+                t = t_value
+
+                fake = (
+                    alpha_dash(t)**0.5 * x_theta + 
+                    (1 - alpha_dash(t))**0.5 * epsilon_theta
+                )
+
+                prediction = tf.cast(
+                    denoiser((tf.cast(fake, preferred_type), tf.constant([t]))), 
+                    tf.float32
+                )
+
+                if ordinary_differential_equation:
+                    x_theta = (
+                        prediction * (1 - alpha_dash(t))**0.5 -
+                        fake * (1 - alpha_dash(t - 1))**0.5
+                    ) / (
+                        alpha_dash(t - 1)**0.5 * 
+                        (1 - alpha_dash(t))**0.5 -
+                        alpha_dash(t)**0.5 *
+                        (1 - alpha_dash(t - 1))**0.5
+                    )
+                    fake = 2 * fake - prediction # TODO
+                else:
+                    if predict_x:
+                        x_theta = prediction
+                        epsilon_theta = (
+                            fake - alpha_dash(t)**0.5 * x_theta
+                        ) / (1 - alpha_dash(t))**0.5
+                        
+                    else:
+                        if predict_scaled_epsilon:
+                            epsilon_theta = (
+                                prediction / (1 - alpha_dash(t))**0.5
+                            )
+                            scaled_epsilon = prediction
+                        else:
+                            epsilon_theta = prediction
+                            scaled_epsilon = (
+                                prediction * (1 - alpha_dash(t))**0.5
+                            )
+                        x_theta = (
+                            fake - scaled_epsilon
+                        ) / alpha_dash(t)**0.5
+
+        # backward diffusion
+        # TODO: apply transformations to epsilon_theta
+
+        pixelated = tf.keras.layers.UpSampling2D(4, interpolation='nearest')(
+            tf.nn.avg_pool2d(epsilon_theta, 4, 4, 'SAME')
+        )
+
+        shifted = tf.roll(tf.roll(epsilon_theta, 1, 1), 1, 2)
+
+        quantisation_error = tf.reduce_sum(tf.math.squared_difference(
+            epsilon_theta[..., None, :], dictionary
+        ), -1)
+        quantised = tf.gather(
+            dictionary[None, ...], tf.argmin(quantisation_error, -1), 
+            batch_dims=3
+        )
+
+        fake = tf.concat([epsilon_theta, pixelated, shifted, quantised], 0)
+
+        fake = tf.concat([example[0, ...], fake], 0)
+
+        x_theta = fake
+        epsilon_theta = fake
 
         for t_value in range(steps, 0, -1): # smallest t = 1
             t = t_value
+
+            fake = (
+                alpha_dash(t)**0.5 * x_theta + 
+                (1 - alpha_dash(t))**0.5 * epsilon_theta
+            )
+
             prediction = tf.cast(
-                denoiser(tf.cast(fake, preferred_type)), tf.float32
+                denoiser((tf.cast(fake, preferred_type), tf.constant([t]))), 
+                tf.float32
             )
 
             if ordinary_differential_equation:
@@ -338,21 +459,22 @@ def log_sample(epochs, logs):
                         fake - scaled_epsilon
                     ) / alpha_dash(t)**0.5
 
-                if t > 0:
-                    fake = (
-                        alpha_dash(t - 1)**0.5 * x_theta + 
-                        (1 - alpha_dash(t - 1))**0.5 * epsilon_theta
-                    )
+            # epsilon_theta' = 
+            #     f(a_t**0.5 * x_theta + (1 - a_t)**0.5 * epsilon_theta)
+            # x_theta' = 
+            #     x_theta + (epsilon_theta - epsilon_theta') * 
+            #     (1 - a_t)**0.5 / a_t**0.5
+            # last term is very large for t close to 1
 
             if t == steps:
-                tf.summary.image('step_1', x_theta * 0.5 + 0.5, epochs, 4)
+                tf.summary.image('step_1', x_theta * 0.5 + 0.5, epochs, 10)
             if t == steps // 4:
-                tf.summary.image('step_0.25', x_theta * 0.5 + 0.5, epochs, 4)
+                tf.summary.image('step_0.25', x_theta * 0.5 + 0.5, epochs, 10)
             if t == 2 * steps // 4:
-                tf.summary.image('step_0.5', x_theta * 0.5 + 0.5, epochs, 4)
+                tf.summary.image('step_0.5', x_theta * 0.5 + 0.5, epochs, 10)
             if t == 3 * steps // 4:
-                tf.summary.image('step_0.75', x_theta * 0.5 + 0.5, epochs, 4)
-        tf.summary.image('fake', x_theta * 0.5 + 0.5, epochs, 4)
+                tf.summary.image('step_0.75', x_theta * 0.5 + 0.5, epochs, 10)
+        tf.summary.image('fake', x_theta * 0.5 + 0.5, epochs, 10)
 
 if __name__ == "__main__": 
     day = datetime.datetime.now().strftime("%Y%m%d")
